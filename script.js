3430553a08e7f2fd24e701717273680961d1d88f
Jacob <j31c0@hotmail.com>
2022-12-17 15:13:48 -0500

    Create 16x16 grid

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const body = document.querySelector('body');
 const container = document.createElement('div');
 container.setAttribute('id','container');
 body.appendChild(container);
+
+for( let i = 0; i < (16*16) ; i++) {
+    const squares = document.createElement('div');
+    squares.classList.toggle('square');
+    container.appendChild(squares);
+}
