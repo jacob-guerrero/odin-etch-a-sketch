b4382e070e359d4d8cccc6226b9f06dfada30824
Jacob <j31c0@hotmail.com>
2022-12-17 21:44:22 -0500

    Add hover effect

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const container = document.createElement('div');
 container.setAttribute('id','container');
 body.appendChild(container);
 
+
 for( let i = 0; i < (16*16) ; i++) {
     const squares = document.createElement('div');
-    squares.classList.toggle('square');
+    squares.classList.toggle('squares');
     container.appendChild(squares);
 }
+
+
+const squares = document.querySelectorAll('.squares');
+squares.forEach((square) => {
+    square.addEventListener('mouseenter', () => {
+        square.classList.add('hover');
+    });
+})
