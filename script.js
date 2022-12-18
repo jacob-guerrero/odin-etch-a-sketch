e914629fb4cfccd8cc95a23e9f7a3d9effce1504
Jacob <j31c0@hotmail.com>
2022-12-18 14:53:00 -0500

    Add function to restart grid

--- script.js
+++ script.js
@@ -9,16 +9,41 @@ for( let i = 0; i < (16*16) ; i++) {
     square.classList.toggle('squares');
     container.appendChild(square);
 }
+hover();
 
-const squares = document.querySelectorAll('.squares');
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', () => {
-        square.classList.add('hover');
+function hover() {
+    const squares = document.querySelectorAll('.squares');
+    squares.forEach((square) => {
+        square.addEventListener('mouseenter', () => {
+            square.classList.add('hover');
+        });
     });
-})
+}
 
 const button = document.createElement('button');
 button.textContent = 'New Grid';
 button.classList.toggle('btn');
 body.insertBefore(button, container);
 
+button.addEventListener('click', () => {
+    let gridSize = prompt('Grid Size (1-100)', '16');
+    deleteGrid();
+    updateSize(gridSize);
+    hover();
+})
+
+function deleteGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function updateSize(gridSize) {
+    for( let i = 0; i < (gridSize*gridSize) ; i++) {
+        const square = document.createElement('div');
+        square.classList.toggle('squares');
+        container.appendChild(square);
+    }
+}
+
+//width and height fix
