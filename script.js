940d6dd01ea8062b694dd04725387d602e1e24a9
Jacob <j31c0@hotmail.com>
2022-12-19 12:15:24 -0500

    Add control buttons

--- script.js
+++ script.js
@@ -3,10 +3,39 @@ const container = document.createElement('div');
 container.setAttribute('id','container');
 body.appendChild(container);
 
+const controlColor = document.createElement('div');
+controlColor.setAttribute('class','controls');
+body.insertBefore(controlColor,container);
+
+const buttonBlack = document.createElement('button');
+buttonBlack.textContent = 'Black';
+buttonBlack.classList.toggle('btn');
+controlColor.appendChild(buttonBlack);
+
+const buttonRgb = document.createElement('button');
+buttonRgb.textContent = 'Rainbow';
+buttonRgb.classList.toggle('btn');
+controlColor.appendChild(buttonRgb);
+
+const buttonShade = document.createElement('button');
+buttonShade.textContent = 'Shades';
+buttonShade.classList.toggle('btn');
+controlColor.appendChild(buttonShade);
+
+const controlGrid = document.createElement('div');
+controlGrid.setAttribute('class','controls');
+body.appendChild(controlGrid);
+
 const button = document.createElement('button');
 button.textContent = 'New Grid';
 button.classList.toggle('btn');
-body.insertBefore(button, container);
+controlGrid.appendChild(button);
+
+const buttonClear = document.createElement('button');
+buttonClear.textContent = 'Clear';
+buttonClear.classList.toggle('btn');
+controlGrid.appendChild(buttonClear);
+
 
 updateSize(16);
 hover();
@@ -31,8 +60,8 @@ function updateSize(gridSize) {
     for( let i = 0; i < (gridSize * gridSize) ; i++) {
         const square = document.createElement('div');
         square.classList.toggle('squares');
-        square.setAttribute('style', `width: calc(40vw / ${gridSize}); 
-                            height: calc(40vw / ${gridSize})`);
+        square.setAttribute('style', `width: calc(50vw / ${gridSize}); 
+                            height: calc(50vw / ${gridSize})`);
         container.appendChild(square);
     }
 }
